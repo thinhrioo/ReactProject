a96e59d73e64d06631d46995285630a315ac9bb4
thinhrio <ducthinh.0203.a2@gmail.com>
2024-04-04 17:40:11 +0700

    design form

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+function App () {
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title:</label>
+            <input
+              type="text"
+
+              placeholder="What's the title of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <label>Description:</label>
+            <input
+              type="text" 
+              placeholder="What's the description of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <button
+              className="primary-btn"
+              type="button"
+
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="btn-area">
+          <button
+            
+          >
+            To Do
+          </button>
+          <button
+            
+          >
+            Completed
+          </button>
+        </div>
+        {/* <div className="todo-list">
+
+          {isCompletedScreen === false &&
+            allTodos.map ((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+
+                </div>
+                <div>
+                  <AiOutlineDelete
+                    title="Delete?"
+                    className="icon"
+                    onClick={() => handleToDoDelete (index)}
+                  />
+                  <BsCheckLg
+                    title="Completed?"
+                    className=" check-icon"
+                    onClick={() => handleComplete (index)}
+                  />
+                </div>
+              </div>
+            ))}
+
+          {isCompletedScreen === true &&
+            completedTodos.map ((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  <p> <i>Completed at: {item.completedOn}</i></p>
+                </div>
+                <div>
+                  <AiOutlineDelete
+                    className="icon"
+                    onClick={() => handleCompletedTodoDelete (index)}
+                  />
+                </div>
+              </div>
+            ))}
+        </div> */}
+      </div>
     </div>
   );
 }
